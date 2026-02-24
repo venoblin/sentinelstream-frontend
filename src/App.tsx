c819d7c251bdfdcb5cc87d55698ab4a1abcfb5f6
Jonathan <villagomezhjonathan@gmail.com>
2026-02-23 22:06:39 -0500

    added salt design system to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { Button, StackLayout, Text } from '@salt-ds/core'
+import { NotificationIcon } from '@salt-ds/icons'
 
 function App() {
   return (
     <>
       <h1>SentinelStream</h1>
+
+      <StackLayout gap={2} style={{ padding: 20 }}>
+        <Text styleAs="h1">SentinelStream Dashboard</Text>
+        <Button variant="cta">
+          <NotificationIcon /> Acknowledge Fraud Alert
+        </Button>
+      </StackLayout>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
+import '@fontsource/open-sans/300.css'
+import '@fontsource/open-sans/300-italic.css'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/400-italic.css'
+import '@fontsource/open-sans/500.css'
+import '@fontsource/open-sans/500-italic.css'
+import '@fontsource/open-sans/600.css'
+import '@fontsource/open-sans/600-italic.css'
+import '@fontsource/open-sans/700.css'
+import '@fontsource/open-sans/700-italic.css'
+import '@fontsource/open-sans/800.css'
+import '@fontsource/open-sans/800-italic.css'
+import '@fontsource/pt-mono/400.css'
+import '@salt-ds/theme/index.css'
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { SaltProvider } from '@salt-ds/core'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <SaltProvider>
+      <App />
+    </SaltProvider>
+  </StrictMode>
 )
